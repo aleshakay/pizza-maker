2d6f9779e0970bfc572a6867ab5b7973b9665385
alesha reed <aleshak83@gmail.com>
2019-09-24 19:59:19 -0500

    added cheese buttons

--- javascripts/components/cheese.js
+++ javascripts/components/cheese.js
@@ -1,9 +1,42 @@
 import printToDom from '../helpers/utilities.js';
 
+const cheese = [
+    {
+    id :'cheese1',
+    name : 'swiss',
+    price : '50',
+    },
+    {
+    id :'cheese2',
+    name : 'colby',
+    price : '2',
+    },
+    {
+    id :'cheese3',
+    name : 'cheddar',
+    price : '5',
+    },
+    {
+    id :'cheese4',
+    name : 'bleu',
+    price : '3',
+    },
+]
+
+
 const cheeseMaker = () => {
-    let domString = 'cheese'
-    printToDom.printToDom('final-order',domString)
+    let domString = '';
+    for (let i=0; i < cheese.length; i++){
+        domString +=`
+    <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id=${cheese[i].id}>
+    <label class="form-check-label" for=${cheese[i].id}>${cheese[i].name}</label>
+    </div>`;
+    }
+    printToDom.printToDom('cheese-counter',domString)
 
 }
+
+
 
 export default { cheeseMaker }
